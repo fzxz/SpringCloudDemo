ae4269dadb2a3f2496447d410e098a7a4e2dcc6f
zhangningwei <46161524@qq.com>
2017-07-30 23:57:07 +0800

    Initial commit

---
+++ feign-api/src/main/java/cn/superdestroy/feignapi/FeignApiApplication.java
@@ -0,0 +1,12 @@
+package cn.superdestroy.feignapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignApiApplication.class, args);
+	}
+}
---
+++ zuul-gateway/src/main/java/cn/superdestroy/zuulgateway/ZuulGatewayApplication.java
@@ -0,0 +1,12 @@
+package cn.superdestroy.zuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+}
